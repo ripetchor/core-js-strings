009c1f7107c0b591e3c8bfcfad95bdc29c11070d
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2024-09-09 16:02:11 +0500

    feat: add solution for formatTime

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -241,8 +241,10 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const minutesStr = String(minutes).padStart(2, 0);
+  const secondsStr = String(seconds).padStart(2, 0);
+  return `${minutesStr}:${secondsStr}`;
 }
 
 /**
