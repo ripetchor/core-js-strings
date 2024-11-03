e19098daf1b6d4f0dc2f704685f1138de78a8a90
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2024-11-03 18:04:37 +0500

    feat: add solution for countVowels

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -306,8 +306,9 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const match = str.match(/[aeiouy]/gi);
+  return match ? match.length : 0;
 }
 
 /**
