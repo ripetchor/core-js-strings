0d669a5c1431980266811a407b01281509af5838
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2024-11-03 18:13:25 +0500

    feat: add solution for invertCase

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -382,8 +382,14 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i += 1) {
+    const char = str[i];
+    result +=
+      char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase();
+  }
+  return result;
 }
 
 /**
