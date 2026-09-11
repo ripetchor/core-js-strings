2805ecf4f7ab5587221944f0a3e60c36bfa0472f
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2026-09-11 12:56:55 +0500

    feat: solve formatTime

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -243,8 +243,11 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const mm = minutes.toString().padStart(2, 0);
+  const ss = seconds.toString().padStart(2, 0);
+
+  return `${mm}:${ss}`;
 }
 
 /**
