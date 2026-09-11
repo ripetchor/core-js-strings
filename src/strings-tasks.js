ae3902ba337e5e056b70eddecca3e140750bf68e
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2026-09-11 13:09:16 +0500

    feat: solve isPalindrome

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -326,8 +326,10 @@ function countVowels(str) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const normalizedString = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+  return normalizedString === normalizedString.split('').reverse().join('');
 }
 
 /**
